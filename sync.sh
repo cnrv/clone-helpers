6eb6b6474235c5402109f69a9104d02ea6697fb4
Alex Guo <xfguo@credosemi.com.cn>
2017-04-28 16:52:46 +0800

    sync with travis-ci matrix.
    Signed-off-by: Alex Guo <xfguo@credosemi.com.cn>

--- sync.sh
+++ sync.sh
@@ -14,37 +14,17 @@ function sync_repo {
     echo
 
     if [ ! -e $WORK/$PROJ ]; then
-        git clone $ORIG_URL/$PROJ $WORK/$PROJ
+        git clone --verbose $ORIG_URL/$PROJ $WORK/$PROJ
     fi
 
     cd $WORK/$PROJ
-    git fetch
-    git push $CNRV_URL/$PROJ +refs/remotes/origin/*:refs/heads/* +refs/tags/*:refs/tags/*
+    git fetch --verbose
+    git push --verbose $CNRV_URL/$PROJ +refs/remotes/origin/*:refs/heads/* +refs/tags/*:refs/tags/*
     cd -
 }
 
 if [ "x$WORK" = "x" ]; then
     echo "WORK is not defined."
 else
-    sync_repo   riscv-test-env       https://github.com/riscv   git@git.oschina.net:cnrv
-    sync_repo   riscv-tests          https://github.com/riscv   git@git.oschina.net:cnrv
-    sync_repo   riscv-pk             https://github.com/riscv   git@git.oschina.net:cnrv
-    sync_repo   riscv-opcodes        https://github.com/riscv   git@git.oschina.net:cnrv
-    sync_repo   riscv-llvm           https://github.com/riscv   git@git.oschina.net:cnrv
-    sync_repo   riscv-clang          https://github.com/riscv   git@git.oschina.net:cnrv
-    sync_repo   riscv-isa-sim        https://github.com/riscv   git@git.oschina.net:cnrv
-    sync_repo   riscv-newlib         https://github.com/riscv   git@git.oschina.net:cnrv
-    sync_repo   riscv-dejagnu        https://github.com/riscv   git@git.oschina.net:cnrv
-    sync_repo   riscv-glibc          https://github.com/riscv   git@git.oschina.net:cnrv
-    sync_repo   riscv-gcc            https://github.com/riscv   git@git.oschina.net:cnrv
-    sync_repo   riscv-binutils-gdb   https://github.com/riscv   git@git.oschina.net:cnrv
-    sync_repo   riscv-gnu-toolchain  https://github.com/riscv   git@git.oschina.net:cnrv
-    sync_repo   riscv-fesvr          https://github.com/riscv   git@git.oschina.net:cnrv
-    sync_repo   riscv-tools          https://github.com/riscv   git@git.oschina.net:cnrv
-
-    sync_repo   riscv-torture        https://github.com/ucb-bar git@git.oschina.net:cnrv
-    sync_repo   berkeley-hardfloat   https://github.com/ucb-bar git@git.oschina.net:cnrv
-    sync_repo   firrtl               https://github.com/ucb-bar git@git.oschina.net:cnrv
-    sync_repo   chisel3              https://github.com/ucb-bar git@git.oschina.net:cnrv
-    sync_repo   rocket-chip          https://github.com/ucb-bar git@git.oschina.net:cnrv
+    sync_repo  $PRJ $SRC $DST
 fi
