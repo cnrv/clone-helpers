178c6ae202f2359f0b028dfb29ced892b6557783
Alex Guo <xfguo@credosemi.com.cn>
2017-05-16 22:37:33 +0800

    try create all projects automaticlly and sync them.
    Signed-off-by: Alex Guo <xfguo@credosemi.com.cn>

--- sync.sh
+++ sync.sh
@@ -4,24 +4,24 @@
 
 function sync_repo {
     PROJ="$1"
-    ORIG_URL="$2"
+    SRC_ORIG="$2"
     CNRV_URL="$3"
 
     echo "------------------------------------------------------------------------------"
     echo "Sync Project: $PROJ"
-    echo "  From: $ORIG_URL/$PROJ"
+    echo "  From: $SRC_ORIG/$PROJ"
     echo "  To:   CNRV-MIRROR/$PROJ"
     echo
 
     if [ ! -e $WORK/$PROJ ]; then
-        git clone --verbose $ORIG_URL/$PROJ $WORK/$PROJ
+        git clone --verbose https://github.com/$SRC_ORIG/$PROJ $WORK/$PROJ
     fi
 
     cd $WORK/$PROJ
     git fetch --verbose
 
     echo "PUSH to cnrv"
-    git push -q $CNRV_URL/$PROJ +refs/remotes/origin/*:refs/heads/* +refs/tags/*:refs/tags/*
+    git push -q $CNRV_URL/cnrv-$SRC_ORIG/$PROJ +refs/remotes/origin/*:refs/heads/* +refs/tags/*:refs/tags/*
 }
 
 if [ "x$WORK" = "x" ]; then
