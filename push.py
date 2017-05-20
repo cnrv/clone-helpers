9c1919049ddb780f5329d9a3f7bb3d7310938beb
Alex Guo <xfguo@credosemi.com.cn>
2017-05-20 13:13:56 +0800

    smart push, cache remote refs and reuse dup ref.
    Signed-off-by: Alex Guo <xfguo@credosemi.com.cn>

--- push.py
+++ push.py
@@ -1,11 +1,22 @@
 from git import Repo
 
-INC_STEP = 1000
+INC_STEP = 10
 
 repo = Repo(".")
+cnrv_remote_ref_sets = set()
+
+print "Cache all cnrv remote cache"
+for ref in repo.refs:
+	if ref.name.startswith("cnrv/") and ref.name != "cnrv/HEAD":
+		print "\tprocessing... branch", ref.name
+		commits = repo.git.rev_list(ref.name).splitlines()
+		cnrv_remote_ref_sets |= set(commits)
+
+print "Start generate push script"
 with open("push.sh", 'w') as f:
 	for ref in repo.refs:
 		if ref.name.startswith("origin/") and ref.name != "origin/HEAD":
+			print "\tprocessing... branch", ref.name
 			f.write("echo %s\n" % ("-" * 80))
 			f.write("echo push branch: %s\n" % ref.name)
 
@@ -27,4 +38,6 @@ with open("push.sh", 'w') as f:
 				push_commits.append(commits[-1])
 				push_commits.reverse()
 				for push_commit in push_commits:
+					if push_commit in cnrv_remote_ref_sets and push_commit != push_commits[-1]:
+						continue
 					f.write("git push -v %s %s:refs/heads/%s\n" % ('cnrv', push_commit, ref_wo_origin))
